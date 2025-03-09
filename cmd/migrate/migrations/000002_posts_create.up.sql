a821c894a73279202ccd911ce8bfead380a79674
karthik.bhandary <karthik.bhandary@kfintech.com>
2025-03-09 15:07:17 +0000

    Migrations

---
+++ cmd/migrate/migrations/000002_posts_create.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS posts (
+    id BIGSERIAL PRIMARY KEY,
+    title text NOT NULL,
+    user_id BIGINT NOT NULL,
+    content text NOT NULL,
+    created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW()
+)
